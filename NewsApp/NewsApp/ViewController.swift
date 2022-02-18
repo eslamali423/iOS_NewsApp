f55fd6d3acb31bffc0b3eef685bc4cb54b708c89
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-02-18 21:19:39 +0200

    initial commit

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Eslam Ali  on 18/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
